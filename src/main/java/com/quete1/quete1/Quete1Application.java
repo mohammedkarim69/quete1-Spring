73223107b2f9a879df0eebe8b059ca52b82080a9
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-03-13 13:41:44 +0100

    first commit

---
+++ src/main/java/com/quete1/quete1/Quete1Application.java
@@ -0,0 +1,16 @@
+package com.quete1.quete1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class Quete1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quete1Application.class, args);
+	}
+
+}
